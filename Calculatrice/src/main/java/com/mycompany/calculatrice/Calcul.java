bf526337b20ee4b4c0efe8dd678befb16075cc2d
Barthezpro <bartheztonlio@gmail.com>
2025-04-25 17:05:52 +0300

    ce code nous donne un exemple de calculatrice

---
+++ Calculatrice/src/main/java/com/mycompany/calculatrice/Calcul.java
@@ -0,0 +1,159 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculatrice;
+
+import  java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author PC
+ */
+public class Calcul{
+    double operande1;
+    double operande2;
+    double result;
+    String cal;
+    public Calcul(){
+        JFrame frame=new JFrame("Calculatrice");
+        frame.setLayout(new BorderLayout() );
+        frame.setBounds(300, 300, 400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+        JPanel resultpanel=new JPanel();
+        resultpanel.setBackground(Color.LIGHT_GRAY);
+        resultpanel.setLayout(new BorderLayout(15,5));
+        resultpanel.setPreferredSize(new Dimension(400, 50));
+        JLabel resultat=new JLabel();
+        resultat.setFont(new Font("Arial",Font.BOLD, 20));
+        resultpanel.add(resultat,BorderLayout.EAST);
+        frame.add(resultpanel,BorderLayout.NORTH);
+        
+        
+        JFileChooser file=new JFileChooser();
+        file.setCurrentDirectory(new File("C:\\Users\\PC\\Desktop\\github1\\Java-Semestre2\\Calculatrice"));
+        file.showOpenDialog(frame);
+        try {
+            FileReader fr=new FileReader(file.getSelectedFile().getAbsoluteFile());
+            BufferedReader br =new BufferedReader(fr);
+            String line=br.readLine();
+            String line1="";
+            while(line!=null){
+                System.out.println(line);
+                line1=line;
+                line=br.readLine();
+            }
+            br.close();
+            String parca[]=line1.split(",");
+            int k=0;
+            JPanel panel=new JPanel();
+            panel.setLayout(new GridLayout(5,4,3,3));
+            for(int i=0;i<5;i++){
+                for(int j=0;j<4;j++){
+                    JButton button=new JButton(parca[k]);
+                    panel.add(button);
+                    k++;
+                    //button.addActionListener((ActionListener) this);
+                    button.addActionListener(new ActionListener(){
+                         public void actionPerformed(ActionEvent evt){
+                            JButton but= (JButton)evt.getSource(); 
+                            if(but.getText().equals("/")){
+                                result=operande1/operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("*")){
+                                result=operande1*operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("-")){
+                                result=operande1-operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            /*else if(but.getText().equals("1")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }
+                            else if(but.getText().equals("+")){
+                                result=operande1+operande2;
+                                cal=""+result;
+                                resultat.setText(cal);
+                            }*/
+                        }
+                    
+                    
+                    
+                    });
+                }
+            }
+            frame.add(panel);
+            frame.setVisible(true);
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(frame, "Fichier non trouver");
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(frame, "Lecture impossible");
+        }
+        
+    }
+    
+}
---
+++ Calculatrice/src/main/java/com/mycompany/calculatrice/Calculatrice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatrice;
+
+/**
+ *
+ * @author PC
+ */
+public class Calculatrice {
+
+    public static void main(String[] args) {
+        Calcul calcul=new Calcul();
+    }
+}
